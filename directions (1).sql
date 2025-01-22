751dfd30e1f5b4c0c33d6fcabe17d753996aa072
Renad Hameed Alharbi <renad.alharbi274@gmail.com>
2025-01-22 18:23:55 +0300

    Add files via upload

---
+++ directions (1).sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 21 يناير 2025 الساعة 16:57
+-- إصدار الخادم: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot_controller_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `directions`
+--
+
+CREATE TABLE `directions` (
+  `id` int(11) NOT NULL,
+  `direction` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `directions`
+--
+
+INSERT INTO `directions` (`id`, `direction`, `timestamp`) VALUES
+(1, 'Right', '2025-01-21 15:42:28'),
+(2, 'Backward', '2025-01-21 15:46:10'),
+(3, 'Left', '2025-01-21 15:47:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `directions`
+--
+ALTER TABLE `directions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `directions`
+--
+ALTER TABLE `directions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
